a62b2b2217e3f93a4eb6e1bf127b5310b006759e
Luis Antonio Mucavele <mucaveleantonioluis@gmail.com>
2025-04-05 16:37:50 +0200

    primeiro commit

---
+++ assets/js/nav.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Botão Menu
+    document.getElementById('btn-menu').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('menu-content').style.display = 'block';
+        document.getElementById('promo-content').style.display = 'none';
+        document.querySelector('.cardapio h1').textContent = 'Menu';
+        this.classList.add('active');
+        document.getElementById('btn-promo').classList.remove('active');
+    });
+    
+    // Botão Promoções
+    document.getElementById('btn-promo').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('menu-content').style.display = 'none';
+        document.getElementById('promo-content').style.display = 'block';
+        document.querySelector('.cardapio h1').textContent = 'Promoções';
+        this.classList.add('active');
+        document.getElementById('btn-menu').classList.remove('active');
+    });
+});
